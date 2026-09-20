f02b0496caf02e99027edc250a74fb2d01ac4266
kingjh9897-cmd <kingjh9897@gmail.com>
2026-09-20 17:43:14 +0200

    Build no-op diagnostic tweak

--- Tweak/NewTweak/build.sh
+++ Tweak/NewTweak/build.sh
@@ -9,20 +9,16 @@ SDK="$(xcrun --sdk iphoneos --show-sdk-path)"
 clang \
   -arch arm64 \
   -dynamiclib \
-  -fobjc-arc \
-  -miphoneos-version-min=15.0 \
+  -miphoneos-version-min=11.0 \
   -isysroot "$SDK" \
-  -framework UIKit \
-  -framework Foundation \
-  -framework CoreGraphics \
   -install_name @rpath/libloader.framework/libloader \
-  "$ROOT/StableMenuTweak.m" \
+  "$ROOT/NoOpTweak.c" \
   -o "$FRAME/libloader"
-cp "$ROOT/Info.plist" "$FRAME/Info.plist"
+cp "$ROOT/../Original-identisch/libloader.framework/Info.plist" "$FRAME/Info.plist"
 chmod 755 "$FRAME/libloader"
 cd "$OUT"
-/usr/bin/zip -qry "KingTweak_Stable_RightButton.zip" "libloader.framework"
-shasum -a 256 "$FRAME/libloader" "KingTweak_Stable_RightButton.zip" > SHA256SUMS.txt
+/usr/bin/zip -qry "KingTweak_NoOp_Diagnostic.zip" "libloader.framework"
+shasum -a 256 "$FRAME/libloader" "KingTweak_NoOp_Diagnostic.zip" > SHA256SUMS.txt
 file "$FRAME/libloader"
 otool -L "$FRAME/libloader"
 cat SHA256SUMS.txt
